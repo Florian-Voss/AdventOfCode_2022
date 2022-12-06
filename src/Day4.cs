1240c43316e041d4197cbab825dd273aadc201ef
Florian Voss <florian.voss@icomcept.de>
2022-12-06 16:52:31 +0100

    .

--- src/Day4.cs
+++ src/Day4.cs
@@ -1,11 +1,14 @@
 public class Day4
 {
+    public static IEnumerable<int[][]> ToIterable2DIntArray(string input){
+        return input.Trim().Split("\n").Select(x => x.Split(",")).Select(x => x.Select(x => x.Split("-").Select(x => int.Parse(x)).ToArray()).ToArray());
+    }
     public static int SolvePartOne(string input)
     {
-        return input.Trim().Split("\n").Select(x => x.Split(",")).Select(x => x.Select(x => x.Split("-").Select(x => int.Parse(x)).ToArray()).ToArray()).Count(x => (x[0][0] <= x[1][0] && x[0][1] >= x[1][1]) | (x[1][0] <= x[0][0] && x[1][1] >= x[0][1]));
+        return ToIterable2DIntArray(input).Count(x => (x[0][0] <= x[1][0] && x[0][1] >= x[1][1]) | (x[1][0] <= x[0][0] && x[1][1] >= x[0][1]));
     }
     public static int SolvePartTwo(string input)
     {
-        return input.Trim().Split("\n").Select(x => x.Split(",")).Select(x => x.Select(x => x.Split("-").Select(x => int.Parse(x)).ToArray()).ToArray()).Count(x => (x[0][0] >= x[1][0] && x[0][0] <= x[1][1]) | (x[0][1] >= x[1][0] && x[0][1] <= x[1][1]) | (x[1][0] >= x[0][0] && x[1][0] <= x[0][1]) | (x[1][1] >= x[0][0] && x[1][1] <= x[0][1]));
+        return ToIterable2DIntArray(input).Count(x => (x[0][0] >= x[1][0] && x[0][0] <= x[1][1]) | (x[0][1] >= x[1][0] && x[0][1] <= x[1][1]) | (x[1][0] >= x[0][0] && x[1][0] <= x[0][1]) | (x[1][1] >= x[0][0] && x[1][1] <= x[0][1]));
     }
 }
